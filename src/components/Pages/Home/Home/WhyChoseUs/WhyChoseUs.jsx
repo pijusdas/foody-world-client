e58b524ffe24ca2a62dd61b6b5a9224bf452152b
pijusdas <pijusdas.dev@gmail.com>
2023-10-18 19:36:08 +0600

    design done in why choose us section

--- src/components/Pages/Home/Home/WhyChoseUs/WhyChoseUs.jsx
+++ src/components/Pages/Home/Home/WhyChoseUs/WhyChoseUs.jsx
@@ -1,40 +1,45 @@
-import { GiCakeSlice } from "react-icons/gi";
+import { GiCakeSlice, GiDeliveryDrone, GiFlowerStar, GiPayMoney } from "react-icons/gi";
 
 const WhyChoseUs = () => {
     return (
-        <div className=" px-12">
+        <div className=" px-12 mb-20">
             <h1 className="text-center text-sky-500  text-5xl font-bold mb-12">WHY CHOOSE US</h1>
-            <div className=" flex justify-between items-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-between items-center">
                 <div>
-                    <div className=" flex gap-5">
+                    <div className=" flex items-center justify-between gap-5 border-sky-400 border-b-2 pb-9">
                         <div>
-                            <h1 className=" text-lg font-bold tex">QUALITY PRODUCTS</h1>
-                            <p className=" text-end">We guarantee the quality of all the cakes we provide as they are baked using the freshest ingredients.</p>
+                            <h1 className=" text-xl hover:text-sky-500 font-bold text-end mb-4 ">QUALITY PRODUCTS</h1>
+                            <p className=" text-end text-gray-500">We guarantee the quality of all the cakes we provide as they are baked using the freshest ingredients.</p>
                         </div>
-                        <GiCakeSlice />
+                        <GiCakeSlice className=" text-9xl font-bold text-sky-500"/>
                     </div>
-                    <div className=" flex gap-5">
+                    <div className=" flex items-center justify-between gap-5 pt-9">
                         <div>
-                            <h1 className=" text-lg font-bold tex">QUALITY PRODUCTS</h1>
-                            <p className=" text-end">We guarantee the quality of all the cakes we provide as they are baked using the freshest ingredients.</p>
+                            <h1 className=" text-xl hover:text-sky-500 font-bold text-end mb-4">CATERING SERVICE</h1>
+                            <p className=" text-end text-gray-500">Our bakery also provides an outstanding catering service for events and special occasions.</p>
                         </div>
-                        <GiCakeSlice />
+                        <GiFlowerStar className=" text-9xl font-bold text-sky-500"/>
                     </div>
                 </div>
+
+
+                <img src="https://i.ibb.co/mTZcqHL/pexels-pixabay-434283-removebg-preview.png" alt="" />
+
+
                 <div>
-                    <div className=" flex gap-5">
+                    <div className=" flex items-center justify-between gap-5 border-sky-400 border-b-2 pb-9">
                         <div>
-                            <h1 className=" text-lg font-bold tex">QUALITY PRODUCTS</h1>
-                            <p className=" text-start">We guarantee the quality of all the cakes we provide as they are baked using the freshest ingredients.</p>
+                            <h1 className=" text-xl hover:text-sky-500 font-bold text-end mb-4">ONLINE PAYMENT</h1>
+                            <p className=" text-end text-gray-500">We accept all kinds of online payments including Visa, MasterCard, American Express credit cards.</p>
                         </div>
-                        <GiCakeSlice />
+                        <GiPayMoney className=" text-9xl font-bold text-sky-500"/>
                     </div>
-                    <div className=" flex gap-5">
+                    <div className=" flex items-center justify-between gap-5 pt-9">
                         <div>
-                            <h1 className=" text-lg font-bold tex">QUALITY PRODUCTS</h1>
-                            <p className=" text-start">We guarantee the quality of all the cakes we provide as they are baked using the freshest ingredients.</p>
+                            <h1 className=" text-xl hover:text-sky-500 font-bold text-end mb-4">FREE DELIVERY</h1>
+                            <p className=" text-end text-gray-500">All orders submitted by our US clients are delivered for free throughout the United States.</p>
                         </div>
-                        <GiCakeSlice />
+                        <GiDeliveryDrone className=" text-9xl font-bold text-sky-500"/>
                     </div>
                 </div>
             </div>
